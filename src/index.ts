2c2e70b9cd1e5bc9d97e46e9504614d4080bed3f
songwuk <1017301325@qq.com>
2023-02-13 17:46:21 +0800

    chore: upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 /* eslint-disable no-console */
-import { chmodSync, copyFileSync, existsSync, mkdirSync, readFileSync, readdirSync, statSync, writeFileSync } from 'node:fs'
+import { chmodSync, existsSync, mkdirSync, readFileSync, statSync, unlinkSync, writeFileSync } from 'node:fs'
 import { dirname, join, normalize } from 'node:path'
 import { cwd } from 'node:process'
 import { fileURLToPath } from 'node:url'
 const _dirname = dirname(fileURLToPath(new URL(import.meta.url)))
-// Logger
-const log = (msg: string): void => console.log(`s-githooks - ${msg}`)
 const GIT_HOOKS = JSON.parse(readFileSync(join(_dirname, './json/hooks.json'), { encoding: 'utf-8' }))
 /**
  * 检查.git是否存在
  */
-function _checkgitexist() {
-  if (!existsSync('.git'))
+function _checkgitexist(rootPath = cwd()) {
+  if (!existsSync(join(rootPath, './.git')))
     throw new Error('[INFO]: .git can\'t be found')
 }
 /**
@@ -47,17 +45,23 @@ function setHooksConfig(projectRootPath = cwd()) {
   for (const hook of GIT_HOOKS) {
     if (Object.prototype.hasOwnProperty.call(config, hook))
       _setHook(hook, config[hook], projectRootPath)
+    else
+      _removeHooks(hook, projectRootPath)
   }
 }
 function _getConfig(projectRootPath: string) {
   const { packageJsonContent } = _getPackageJson(projectRootPath)
   return packageJsonContent['s-githooks']
 }
+function _removeHooks(hook: string, rootPath = cwd()) {
+  const hookPath = normalize(join(rootPath, './.git/hooks', hook))
+  if (existsSync(hookPath))
+    unlinkSync(hookPath)
+}
 function _setHook(hook: string, command: string, rootPath = cwd()) {
   _checkgitexist()
-  console.log(hook, command, rootPath)
   const hookCommand = `#!/bin/sh\n${command}`
-  const hookDirectory = '.git/hooks/'
+  const hookDirectory = normalize(join(rootPath, './.git/hooks/'))
   const hookPath = normalize(hookDirectory + hook)
   const normalizedHookDirectory = normalize(hookDirectory)
   if (!existsSync(normalizedHookDirectory))
@@ -66,49 +70,6 @@ function _setHook(hook: string, command: string, rootPath = cwd()) {
   chmodSync(hookPath, 0o0755)
   console.log(`[INFO] Successfully set the ${hook} with command: ${command}`)
 }
-/**
- * 读取本地hook放到husky里面去
- */
-export function install(dir = '.husky'): void {
-  console.log(_dirname)
-  _checkgitexist()
-  try {
-    const dirHook = join(_dirname, '../commithooks')
-    if (existsSync(dirHook)) {
-      const readFile = readdirSync(dirHook)
-      if (readFile.length > 0) {
-        readFile.forEach(async (file) => {
-          const sourceFile = join(_dirname, '../commithooks', file)
-          const destPath = join(_dirname, '..', dir, file)
-          copyFileSync(sourceFile, destPath)
-        })
-      }
-      else {
-        console.log('s-githooks - file empty')
-      }
-    }
-  }
-  catch (error) {
-    console.log(`s-githooks - ${error}`)
-    throw error
-  }
-  log('s-githooks installed')
-}
-
-export function readGitHooks() {
-  _checkgitexist()
-  try {
-    const gitHook = join(_dirname, '../.git/hooks')
-    if (existsSync(gitHook)) {
-      const readFile = readdirSync(gitHook)
-      console.log(readFile)
-    }
-  }
-  catch (error) {
-
-  }
-}
-
 export {
   checkGitHooksInDependencies,
   setHooksConfig,
